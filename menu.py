8097e3c8708603bee321554b2f6f4009579e2fb9
MatrixRex <matrixrex05@gmail.com>
2026-01-10 22:56:14 +0600

    removed restart blender function

--- menu.py
+++ menu.py
@@ -8,18 +8,11 @@ def draw_uv_menu(self, context):
         text="Seam Area by Angle (Modal)",
         icon='MOD_SMOOTH'
     )
-def draw_file_menu_restart(self, context):
-    layout = self.layout
-    layout.separator()
-    layout.operator("rextools3.restart_blender", text="Restart Blender", icon='QUIT')
 
 
 def register():
     
     bpy.types.VIEW3D_MT_uv_map.append(draw_uv_menu)
-    bpy.types.TOPBAR_MT_file.append(draw_file_menu_restart)
 
 def unregister():
     bpy.types.VIEW3D_MT_uv_map.remove(draw_uv_menu)
-    bpy.types.TOPBAR_MT_file.remove(draw_file_menu_restart)
-    
